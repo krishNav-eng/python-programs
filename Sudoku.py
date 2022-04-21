423a5ed115a8c13a55e9bc05401b9f624af884c6
krishNav-eng <71910691+krishNav-eng@users.noreply.github.com>
2022-04-21 21:27:42 +0530

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -1,53 +1,74 @@
-import random as rd
-from time import time_ns
+from random import randint, shuffle
 
 class SudukoPuzzleMaker:
   def __init__(self):
-    self.dim = 9
-    self.board = [[0 for _ in range(self.dim)] for _ in range(self.dim)]
+    self.board = []
+    l = list(range(1, 10))
+    shuffle(l)
+    self.board.append(l)
+    self.board += [[0 for _ in range(9)] for _ in range(8)]
 
-  def makeBoard(self):
-    st = time_ns()
+  def make(self):
+    find = self.find_empty()
+    if not find:
+      return True
+    else:
+      r, c = find
+    
+    tries = set()
 
-    for i in range(self.dim):
-      for j in range(self.dim):
-        num = rd.randint(1, self.dim)
-        if num == 0:
-          print("0")
-          self.board[j][i] = 0
+    while len(tries) != 9:
+      n = randint(1, 9)
+      tries.add(n)
+      if self.possible(n, r, c):
+        self.board[r][c] = n
 
-        else :
-          if self.possible(i, j, num):
-            self.board[i][j] = num
+        if self.make():
+          return True
 
-    ft = time_ns()
-    tt = ft - st
-    print(f"Time taken: {tt}ns || {tt//100}ms")
+        self.board[r][c] = 0
+  
+  def final_step(self):
+    pos = set()
+    # of Removed
+    n = 0
+    while n <= 30:
+      i, j = randint(0, 8), randint(0, 8)
+      if (i, j) not in pos:
+        pos.add((i, j))
+        pos.add((j, i))
+        self.board[i][j] = 0
+        self.board[j][i] = 0
+        if (i, j) != (j, i):
+          n+=2
+        else:
+          n+=1
 
-  def getBoard(self):
-    return self.board
-
-  def possible(self, row, col, num):
-    for x in range(self.dim):
-      if self.board[row][x] == num:
+  def possible(self, n, r, c):
+    for i in range(9):
+      if self.board[r][i] == n and i != c:
         return False
-      if self.board[x][col] == num:
+      if self.board[i][c] == n and i != r:
         return False
 
-    k = row//3
-    l = col//3
-    for x in range(3):
-      for y in range(3):
-        if self.board[k+x][l+y] == num:
+    x = (r // 3) * 3
+    y = (c // 3) * 3
+    for i in range(x, x+3):
+      for j in range(y, y+3):
+        if self.board[i][j] == n and (i, j) != (r, c):
           return False
 
     return True
 
-  def getSpaceCount(self):
-    counter = 0
-    for i in range(self.dim):
-      for j in range(self.dim):
+  def find_empty(self):
+    for i in range(9):
+      for j in range(9):
         if self.board[i][j] == 0:
-          counter += 1
+          return (i, j)
 
-    return counter
+    return None
+  
+  
+
+  
+# Thank you :)
